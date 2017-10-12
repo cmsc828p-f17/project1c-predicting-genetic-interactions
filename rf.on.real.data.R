55fb7e824c49e3b9814672e6c8f6c489fb5606f1
Kenneth Cheng <cheng.ky@hotmail.com>
2017-10-12 10:21:16 -0400

    on server

--- rf.on.real.data.R
+++ rf.on.real.data.R
@@ -4,19 +4,7 @@ setwd("/cbcb/project2-scratch/kycheng/project1c-predicting-genetic-interactions"
 library(data.table)
 library(ranger)
 
-# gi scores, already unique pairs
-dat <- fread("data/collins-sc-emap-gis.tsv")
-setnames(dat, c("gene1","gene2","gi.score","gi.type"))
-# gene to GO term mapping
-go.dat <- fread("data/GO.tsv")
-go.dat <- go.dat[Gene %in% dat[, c(gene1,gene2)]]
-dat <- dat[gene1 %in% go.dat$Gene & gene2 %in% go.dat$Gene]
-go.dat <- split(go.dat$Gene, go.dat$Term)
-# genes list
-gl <- get.genes.list(dat)
-# get features (slow)
-dat <- cbind(dat, get.features(go.dat, gl))
-save(dat, file="data.RData")
+load("data.RData") # dat
 
 # number of samples
 N <- nrow(dat)
@@ -27,7 +15,7 @@ cv.result <- lapply(1:4, function(i) {
   train <- folds!=i
   test <- dat[!train]
   set.seed(1)
-  rf <- ranger(Score ~ ., data=dat[train, c(-1,-2,-4)], num.trees=500, mtry=floor((length(dat)-4)/3))
+  rf <- ranger(gi.score ~ ., data=dat[train, c(-1,-2,-4)], num.trees=500, mtry=floor((length(dat)-4)/3))
   prd <- predictions(predict(rf, data=test))
   test[, .(gene1, gene2, gi.score.real=gi.score, gi.score.prd=prd)]
 })
